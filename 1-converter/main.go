bc9ddb204a70f9f9fa33e9697ba70132b6b986dd
Andrei Lvov <andrew.lvov@icloud.com>
2025-03-15 13:14:23 +0300

    Второе домашнее задание

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,5 +6,18 @@ func main() {
 	const usdToEur = 0.92
 	const usdToRub = 85.19
 	eurToRub := usdToRub / usdToEur
-	fmt.Println("Eur to Rub :", eurToRub)
+	amount := getUserInput()
+	_ = convert(0, "", "")
+	fmt.Println("Eur to Rub :", amount*eurToRub)
+}
+
+func getUserInput() float64 {
+	var userInput float64
+	fmt.Print("Enter the amount of EUR: ")
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func convert(amount float64, initialCurrency, desiredCurrency string) float64 {
+	return 0
 }
